dc040eb92afaebb798f7db84021c8508da2e18bc
thanetchavalaisakul-cmd <thanetchavalaisakul@gmail.com>
2026-09-18 12:37:58 +0700

    Delete config.js

--- config.js
+++
@@ -1,3 +0,0 @@
-export const SUPABASE_URL = "https://thvusgkxhiwsilipigno.supabase.co";
-export const SUPABASE_PUBLISHABLE_KEY = "sb_publishable_woASkluXHw_PkaoMZgACYA_Ute6Npgv";
-export const APP_NAME = "BNK School OS";
